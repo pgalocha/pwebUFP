00a8296e6f4f8c1815955d08936d684bccc397d5
DESKTOP-TK50MEU\pgalo <pgalocha93@hotmail.com>
2017-01-11 11:16:58 +0000

    no message

--- application/app/Http/Controllers/HomeController.php
+++ application/app/Http/Controllers/HomeController.php
@@ -34,8 +34,9 @@ class HomeController extends Controller
         echo "<pre>";
         print_r($data);
         echo "</pre>";*/
-       // echo $data['name'];
+
             $data = $request->all();
+            die( $data['contact']);
             User::create([
             'name' => $data['name'],
                 'contact' => $data['contact'],
